d45cbb0b1ffcd3a1dee44c0abde5775d53d96eca
HamzaKhan831 <60002643+HamzaKhan831@users.noreply.github.com>
2020-07-10 21:13:51 +0500

    Initial commit

---
+++ fyp/identify_face_video.py
@@ -0,0 +1,71 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow.compat.v1 as tf
+from scipy import misc
+import cv2
+import numpy as np
+import facenet
+import detect_face
+import os
+import time
+import pickle
+
+modeldir = './model/20170511-185253.pb'
+classifier_filename = './class/classifier.pkl'
+npy='./npy'
+train_img="./train_img"
+
+with tf.Graph().as_default():
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
+    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+    with sess.as_default():
+        pnet, rnet, onet = detect_face.create_mtcnn(sess, npy)
+
+        minsize = 20  # minimum size of face
+        threshold = [0.6, 0.7, 0.7]  # three steps's threshold
+        factor = 0.709  # scale factor
+        margin = 44
+        frame_interval = 3
+        batch_size = 1000
+        image_size = 182
+        input_image_size = 160
+        
+        HumanNames = os.listdir(train_img)
+        HumanNames.sort()
+
+        print('Loading Model')
+        facenet.load_model(modeldir)
+        images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+        embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+        phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+        embedding_size = embeddings.get_shape()[1]
+
+
+        classifier_filename_exp = os.path.expanduser(classifier_filename)
+        with open(classifier_filename_exp, 'rb') as infile:
+            (model, class_names) = pickle.load(infile)
+
+        video_capture = cv2.VideoCapture(0)
+        c = 0
+
+        face_locations = []
+        face_encodings = []
+        face_names = []
+        process_this_frame = True
+
+        print('Start Recognition')
+        prevTime = 0
+        while True:
+            ret, frame = video_capture.read()
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            rgb_small_frame = small_frame[:, :, ::-1]
+            
+            if process_this_frame:
+                face_locations = face_recognition.face_locations(rgb_small_frame)
+                face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+                
+                face_names = []
+                
+                
+
